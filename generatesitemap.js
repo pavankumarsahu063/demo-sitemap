db7a8c3df62fe1c25f9dcda951b75e420704b45a
pavankumar063 <pavankumar987321@gmail.com>
2025-05-09 15:48:50 +0530

    first commit

--- generatesitemap.js
+++ generatesitemap.js
@@ -1,3 +1,38 @@
-// import {writeFile} from 'fs/promises';
+import {writeFile} from 'fs/promises';
+import path from 'path';
 
 
+const baseUrl="https://demo-sitemap-app.vercel.app/";
+
+const endPoints=[
+
+    "/",
+    "/about",
+    "/service"
+];
+
+const date =new Date();
+const lastUpdate=date.toISOString().split("T")[0];
+
+
+const siteMap=`
+    <?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+
+
+    endPoints.map((currentPage)=>
+
+        <url>
+            <loc>${baseUrl}${path}</loc>
+            <lastmod>${lastUpdate}</lastmod>
+            <changefreq>daily</changefreq>
+            <priority>03</priority>
+        </url>
+)
+.join("")}
+
+</urlset>`;
+
+await writeFile("publi/sitemap.xml",siteMap);
+console.log("sucessfull");
